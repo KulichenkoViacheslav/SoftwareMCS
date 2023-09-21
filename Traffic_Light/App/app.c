1eb2a90fe2a5cd178344749defaa6618cc8c3706
Viacheslav Kulichenko <Kulichenko.Viacheslav@gmail.com>
2023-09-21 12:06:13 +0300

    Create Trafec Light App

---
+++ Traffic_Light/App/app.c
@@ -0,0 +1,33 @@
+#include "app.h"
+#include "light.h"
+// Variants
+/*
+1. Red
+2. Red + Yelow
+3. Green
+4. Green blink
+5. Yelow
+*/
+
+enum trafic_light_mode
+{
+	yelow_blink = 0,
+	red,
+	red_yelow,
+	green,
+	green_blink,
+	yelow,
+};
+
+void app_init(void)
+{
+	
+}	
+
+void app_run(void)
+{
+	while(1)
+	{
+		
+	}
+}
